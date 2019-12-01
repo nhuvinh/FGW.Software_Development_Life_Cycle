f44984b484e5f2a383046d792f89489401be5bfb
Vinh Hoang <nhuvinh.hoang@gmail.com>
2019-12-01 10:28:10 +0700

    Testing Strings

---
+++ Unit-Testing-Course/TestNinja.UnitTest/HtmlFormatterTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTest
+{
+				[TestFixture]
+				class HtmlFormatterTests
+				{
+								[Test]
+								public void FormatAsBold_WhenCalled_ShouldEncloseStringWithStrongElement()
+								{
+												var formatter = new HtmlFormatter();
+
+												var result = formatter.FormatAsBold("abc");
+
+												// Specific
+												Assert.That(result, Is.EqualTo("<strong>abc</strong>"));
+
+												// More general
+												Assert.That(result, Does.StartWith("<strong>"));
+												Assert.That(result, Does.EndWith("</strong>"));
+												Assert.That(result, Does.Contain("abc"));
+								}
+				}
+}
