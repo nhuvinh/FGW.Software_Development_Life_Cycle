f233c2254be34d4c733b3261b01386caea8f829c
Vinh Hoang <nhuvinh.hoang@gmail.com>
2019-12-01 10:54:22 +0700

    Testing methods that raise an event

--- Unit-Testing-Course/TestNinja.UnitTest/ErrorLoggerTests.cs
+++ Unit-Testing-Course/TestNinja.UnitTest/ErrorLoggerTests.cs
@@ -31,5 +31,22 @@ namespace TestNinja.UnitTest
 
 												Assert.That(() => logger.Log(error), Throws.ArgumentNullException);
 								}
+
+								[Test]
+								public void Log_ValidError_RaiseErrorLoggedEvent()
+								{
+												var logger = new ErrorLogger();
+
+												var id = Guid.Empty;
+
+												logger.ErrorLogged += (sender, args) =>
+												{
+																id = args;
+												};
+
+												logger.Log("a");
+
+												Assert.That(id, Is.Not.EqualTo(Guid.Empty));
+								}
 				}
 }
