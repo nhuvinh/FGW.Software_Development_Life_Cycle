ceac618f67b2422592968e51d5ac0e71a7bade51
Vinh Hoang <nhuvinh.hoang@gmail.com>
2019-12-02 11:46:22 +0700

    Add Mocking

---
+++ Unit-Testing-Course/TestNinja.UnitTest/Mocking/OrderServiceTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+using TestNinja.Mocking;
+
+
+namespace TestNinja.UnitTest.Mocking
+{
+				class OrderServiceTests
+				{
+								[Test]
+								public void PlaceOrder_WhenCalled_StoreTheOrder()
+								{
+												var storage = new Mock<IStorage>();
+												var service = new OrderService(storage.Object);
+
+												var order = new Order();
+												service.PlaceOrder(order);
+
+												storage.Verify(s => s.Store(order));
+								}
+				}
+}
---
+++ Unit-Testing-Course/TestNinja.UnitTest/Mocking/VideoServiceTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTest.Mocking
+{
+				[TestFixture]
+				class VideoServiceTests
+				{
+								[Test]
+								public void ReadVideoTitle_EmptyFile_ReturnError()
+								{
+												var fileReader = new Mock<IFileReader>();
+												fileReader.Setup(fr => fr.Read("video.txt")).Returns("");
+
+												var service = new VideoService(fileReader.Object);
+
+												var result = service.ReadVideoTitle();
+
+												Assert.That(result, Does.Contain("error").IgnoreCase);
+												
+								}
+				}
+}
